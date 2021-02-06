5bec507921f41a4332d5e250cf1ef42237abb0dd
Chris Ratterree <ratt18@hotmail.com>
2021-02-05 20:55:19 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { Jumbotron, Container } from 'reactstrap';
-import './App.css';
+import React, { Component } from "react";
+import { Jumbotron, Container } from "reactstrap";
+import "./App.css";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Jumbotron fluid>
-        <Container fluid>
-          <img src="/public/assets/images/vjd_logo.png" width="600" alt="vegan jerky death match logo"/>
-        </Container>
-      </Jumbotron>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Jumbotron fluid>
+          <Container fluid>
+            <img src="images/logo.png"></img>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
 }
 
 export default App;
