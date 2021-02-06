8dfa7150deb6fc11a33fc4f34672052b97140093
Chris Ratterree <ratt18@hotmail.com>
2021-02-05 20:05:30 -0700

    react setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Jumbotron, Container } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Jumbotron fluid>
+        <Container fluid>
+          <img src="/public/assets/images/vjd_logo.png" width="600" alt="vegan jerky death match logo"/>
+        </Container>
+      </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
