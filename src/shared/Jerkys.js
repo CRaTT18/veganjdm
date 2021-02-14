13b931328d6cfe3608b99b01fb330cf6d8c5909e
Chris Ratterree <ratt18@hotmail.com>
2021-02-14 13:55:47 -0700

    img

--- src/shared/Jerkys.js
+++ src/shared/Jerkys.js
@@ -2,7 +2,7 @@ export const JERKYS = [
     {
         id: 0,
         name: "Louisville Vegan Jerky Co.",
-        image: "../images/louisville_header.jpg",
+        image: "./images/louisville_header.jpg",
         rank: 1,
         flavor: "Smoked Black Pepper",
         taste: "9",
@@ -15,7 +15,7 @@ export const JERKYS = [
     {
         id: 1,
         name:"Primal Spirit Foods Primal Strips",
-        image: "../images/primal_spirit_header.jpg",
+        image: "./images/primal_spirit_header.jpg",
         rank: 2,
         flavor: "Teriyaki",
         taste: "7",
@@ -28,7 +28,7 @@ export const JERKYS = [
     {
         id: 2,
         name:"Solely: Organic Fruit Jerky",
-        image: "../images/solely_header.jpg",
+        image: "./images/solely_header.jpg",
         rank: 3,
         flavor: "Pineapple with Chili & Salt",
         taste: "10",
@@ -41,7 +41,7 @@ export const JERKYS = [
     {
         id: 3,
         name:"Sweet Earth Kung-Pao Plant-Based Jerky",
-        image: "../images/sweet_earth_header.jpg",
+        image: "./images/sweet_earth_header.jpg",
         rank: 4,
         flavor: "Spicy Kung-Pao",
         taste: "7",
@@ -54,7 +54,7 @@ export const JERKYS = [
     {
         id: 4,
         name:"Foreal Foods Cocunut Jerky",
-        image: "../images/foreal_header.jpg",
+        image: "./images/foreal_header.jpg",
         rank: 5,
         flavor: "Original Peppered",
         taste: "2",
