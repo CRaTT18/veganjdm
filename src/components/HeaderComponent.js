c126b671a370f4f513acc1413afd7a2ee63fe914
Chris Ratterree <ratt18@hotmail.com>
2021-02-13 17:01:58 -0700

    collapse & css

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,82 +1,102 @@
 import React, { Component } from "react";
-import { Nav, Navbar, NavItem } from "reactstrap";
+import { Nav, Navbar, NavItem, NavbarToggler, Collapse } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import logo from "../images/logo.png";
 import axe from "../images/axe.png";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false,
+    };
+  }
+
+  toggleNav() {
+    this.setState({ isNavOpen: !this.state.isNavOpen });
+  }
+
   render() {
     return (
       <>
         <Navbar dark sticky="top" expand="md">
           <div className="container">
-            <Nav navbar>
-              <NavItem>
-                <NavLink className="nav-link" to="/home">
-                  <img
-                    src={axe}
-                    className="axe-small"
-                    height="40"
-                    alt="Axe Logo"
-                    href="/"
-                  />{" "}
-                  Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to="/rankings">
-                  <img
-                    src={axe}
-                    className="axe-small"
-                    height="40"
-                    alt="Axe Logo"
-                    href="/"
-                  />{" "}
-                  Rankings
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to="/match">
-                  <img
-                    src={axe}
-                    className="axe-small"
-                    height="40"
-                    alt="Axe Logo"
-                    href="/"
-                  />{" "}
-                  Matches
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to="/aboutus">
-                  <img
-                    src={axe}
-                    className="axe-small"
-                    height="40"
-                    alt="Axe Logo"
-                    href="/"
-                  />{" "}
-                  About
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to="/contactus">
-                  <img
-                    src={axe}
-                    className="axe-small"
-                    height="40"
-                    alt="Axe Logo"
-                    href="/"
-                  />{" "}
-                  Contact
-                </NavLink>
-              </NavItem>
-            </Nav>
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <img
+                      src={axe}
+                      className="axe-small"
+                      height="40"
+                      alt="Axe Logo"
+                      href="/"
+                    />{" "}
+                    Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/rankings">
+                    <img
+                      src={axe}
+                      className="axe-small"
+                      height="40"
+                      alt="Axe Logo"
+                      href="/"
+                    />{" "}
+                    Rankings
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/match">
+                    <img
+                      src={axe}
+                      className="axe-small"
+                      height="40"
+                      alt="Axe Logo"
+                      href="/"
+                    />{" "}
+                    Matches
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <img
+                      src={axe}
+                      className="axe-small"
+                      height="40"
+                      alt="Axe Logo"
+                      href="/"
+                    />{" "}
+                    About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <img
+                      src={axe}
+                      className="axe-small"
+                      height="40"
+                      alt="Axe Logo"
+                      href="/"
+                    />{" "}
+                    Contact
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </Navbar>
         <div className="row m-5">
           <a href="/home" className="mx-auto">
-            <img src={logo} className="img-fluid" width="1000" alt="Vegan jerky deathmatch logo" />
+            <img
+              src={logo}
+              className="img-fluid"
+              width="1000"
+              alt="Vegan jerky deathmatch logo"
+            />
           </a>
         </div>
         <hr />
